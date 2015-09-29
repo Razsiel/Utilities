cb083f5bd1beec11b03a77b27fdbc050fbd8d017
geoffrey <geoffreyarkenbout@gmail.com>
2015-09-29 20:35:13 +0200

    Added a simple Singleton class for inheriting

---
+++ Utilities/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if( _instance == null)
+                    _instance = new Singleton();
+                return _instance;
+            }
+        }
+    }
+}
